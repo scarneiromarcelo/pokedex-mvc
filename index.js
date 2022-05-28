0a85944a4e897b1b3be5a663cdbf0fbf259b5b2a
scarneiromarcelo <s.carneiro.marcelo@gmail.com>
2022-05-28 16:47:34 -0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path"); // Utilizando biblioteca "path" do express para acessar index.js e style.css
 
-
 const app = express(); // Variável que está recebendo o express
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000; // Config porta
 
 app.set("view engine", "ejs"); // Fala para o servidor que a view engine (motor que vai renderizar) é o ejs.
 
-app.use(express.static(path.join(__dirname, "public"))) // Arquivos estáticos (index.js e style.css). 
+app.use(express.static(path.join(__dirname, "public"))); // Arquivos estáticos (index.js e style.css).
 
-const pokedex = [ //Criando os objetos da pokedex
+app.use(express.urlencoded()); //Cliente envia info do input através de JSON. e URLENCODED recebe
+
+const pokedex = [
+  //Criando os objetos da pokedex
   {
     id: 1,
     nome: "Bulbasaur",
@@ -37,14 +39,28 @@ const pokedex = [ //Criando os objetos da pokedex
     tipo: "Water",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
   },
-  
+  {
+    id: 4,
+    nome: "Caterpie",
+    descricao:
+      "For protection, it releases a horrible stench from the antenna on its head to drive away enemies.",
+    tipo: "Bug",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
+  },
 ];
-
 
 //Rotas
 app.get("/", (req, res) => {
   //Acessando a rota "/"
-  res.render("index", {pokedex}); // Já temos a renderização configurada e estamos enviando a nossa const "pokedex" como JSON.
+  res.render("index", { pokedex }); // Já temos a renderização configurada e estamos enviando a nossa const "pokedex" como JSON.
+});
+
+app.post("/add", (req, res) => {
+  const pokemon = req.body;
+
+  pokedex.push(pokemon);
+
+  res.redirect("/"); //Recarrega a página
 });
 
 app.listen(
